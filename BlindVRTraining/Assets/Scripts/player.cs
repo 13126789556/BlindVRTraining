ff5bd86055bd2c2df0543ee3976ddc64cf2e7b57
Luyao <luyaoz3283@gmail.com>
2020-03-06 22:49:47 -0800

    Player controller done

---
+++ BlindVRTraining/Assets/Scripts/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    [SerializeField] private GvrEditorEmulator VREmu;
+    [SerializeField] private float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        //press A to move forward
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 newDir = getUnitFacingDirection();
+            newDir.y = 0;
+            transform.position = transform.position + Time.deltaTime * newDir.normalized * speed; 
+        }
+    }
+
+    //get unit vector of the facing direction
+    public Vector3 getUnitFacingDirection()
+    {
+        return (VREmu.HeadRotation * Vector3.forward).normalized;
+    }
+}
