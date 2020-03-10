2d761823f7b9a1f0c4c981f784a8146d39d69868
Ruonan Chen <chennylan@163.com>
2020-03-09 21:03:20 -0700

    add turn left parallel to the trafic

--- BlindVRTraining/Assets/Scripts/TurnHeadParallel.cs
+++ BlindVRTraining/Assets/Scripts/TurnHeadParallel.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class TurnHeadParallel : MonoBehaviour
 {
     player _player;
+    GameObject _startPosition;
     // Start is call`ed before the first frame update
     void Start(){
         _player = GetComponent<player>();
+        _startPosition = GameObject.FindGameObjectWithTag("StartPosition");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        getAngle();
-        isBackfacing();
+    private void FixedUpdate() {
+        //turn head to track the car sound
+        //TODO: instruction
+
+        //TODO: instruction
+        //turn left side parallel to the traffic
+        comfirmPosition();
     }
+    // Update is called once per frame
 
     public bool isLeftSideParallel()
     {
@@ -23,7 +28,6 @@ public class TurnHeadParallel : MonoBehaviour
     }
 
     public bool isBackfacing(){
-        print(Mathf.Abs((getAngle() - 90f)) < 15f);
         return Mathf.Abs((getAngle() - 90f)) < 15f;
     }
 
@@ -31,15 +35,29 @@ public class TurnHeadParallel : MonoBehaviour
         //angle = x1x2+y1y2 / sqr(x1*x1 + y1*y1) * sqr(x2*x2 + y2*y2)
         Vector3 facing = _player.getUnitFacingDirection();
         //forward (0,1)
-        print(Vector3.forward);
-        float cosAngle = facing.y / Mathf.Sqrt(facing.x * facing.x + facing.y * facing.y);
+        float cosAngle = facing.z / Mathf.Sqrt(facing.x * facing.x + facing.z * facing.z);
         float angle = Mathf.Acos(cosAngle) * Mathf.Rad2Deg;
-        print(angle);
+        print("angle:" + angle);
         return angle;
     }
     
-
     public void comfirmPosition(){
+        if(Input.GetButtonDown("Confirm")){
+            //check if the player is in the right position
+            if(isLeftSideParallel()){
+                //TODO: ADD SOUND
+                print("you got it!");
+            }
+            else{
+                //TODO: ADD SOUND
+                this.transform.rotation = Quaternion.Euler(this.transform.rotation.y, Random.Range(-180f, 180f), this.transform.rotation.y);
+                print("try again");
+            }
+            print("confirm");
+        }
+    }
+
+    public void turnHead2TrackSound(){
         
     }
 }
